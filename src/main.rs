4833d0dad9ac14a923bab7911ccbac2a075dae10
Romando Purba <romando@ecampus.ut.ac.id>
2025-01-31 15:36:14 +0700

    init

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use actix_web::{get, middleware::{DefaultHeaders, Logger}, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .wrap(DefaultHeaders::new().add(("X-Version", "0.0.1")))
+            .service(index)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
+}
+
+#[get("/")]
+async fn index() -> &'static str {
+    "Hello, world!"
+}
