1f0d4005b25f048cf4bc798134884d1e6a8b8660
Romando Purba <romando@ecampus.ut.ac.id>
2025-02-03 16:33:51 +0700

    change to workspaces

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,30 +1,18 @@
 use actix_web::{
-    get,
     middleware::{DefaultHeaders, Logger},
     App, HttpServer,
 };
-use web::route_config;
-
-mod web;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
-
-    HttpServer::new(move || {
+    HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
             .wrap(DefaultHeaders::new().add(("X-Version", "0.0.1")))
-            .service(index)
-            .configure(route_config)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
     .await
-}
-
-#[get("/")]
-async fn index() -> &'static str {
-    "Hello, world!"
 }
